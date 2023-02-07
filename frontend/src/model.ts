d75b03e71aeeaec45fb370c775067a98dcc9f4a5
shuent <shunshun.43@gmail.com>
2023-02-08 07:06:04 +0900

    .

--- frontend/src/model.ts
+++ frontend/src/model.ts
@@ -1,10 +1,4 @@
-export type PostCreate = {
-  uid: string
-  body: string
-  audio_filename: string
-  original_filename: string
-  user_uid: string
-}
+export type PostCreate = Post
 
 export type Post = {
   uid: string
