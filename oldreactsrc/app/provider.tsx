9f29f6e8205b5b6e61c29b4c59b3fd62a84f2b6a
shuent <shunshun.43@gmail.com>
2023-07-08 16:42:39 +0900

    setup remix indie-stack

--- oldreactsrc/app/provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { AuthProvider } from '@/provider/auth_provider'
-
-export default function Provider({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>
-}
--- oldreactsrc/util.ts
+++
@@ -1,2 +0,0 @@
-export const filterObject = (obj: Object, fn: Function) =>
-  Object.fromEntries(Object.entries(obj).filter(([k, v]) => fn(k, v)))
